4a419da3ff0bfe18b4fe355bdb9f692855c3c0d7
TheConnMan <bcconn2112@gmail.com>
2017-11-30 20:26:00 -0500

    Remove unnecessary Promise wrapping

--- ts/WebService.ts
+++ ts/WebService.ts
@@ -7,20 +7,13 @@ const logger = log4js.getLogger();
 class WebService {
 
   public requestUrl(payload): Promise<void> {
-    return new Promise((resolve, reject) => {
-      Request({
-        body: typeof payload.body === 'object' ? JSON.stringify(payload.body) : payload.body,
-        method: (payload.method || 'GET').toUpperCase(),
-        qs: payload.params || {},
-        timeout: payload.timeout || 5000,
-        uri: payload.url
-      }).promise().then((response) => {
-        logger.info(`Response from ${payload.url}: ${response}`);
-        resolve();
-      }).catch((e) => {
-        reject(e);
-      });
-    });
+    return Request({
+      body: typeof payload.body === 'object' ? JSON.stringify(payload.body) : payload.body,
+      method: (payload.method || 'GET').toUpperCase(),
+      qs: payload.params || {},
+      timeout: payload.timeout || 5000,
+      uri: payload.url
+    }).promise() as any;
   }
 }
 
